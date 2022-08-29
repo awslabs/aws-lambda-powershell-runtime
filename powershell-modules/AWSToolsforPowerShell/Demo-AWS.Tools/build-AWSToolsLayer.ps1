e7b193492bc8bca5d7a8ef91a9f397a4aa2aa868
Andrew Pearce <andrew@austoonz.net>
2022-08-29 13:38:22 -0700

    Fixed a bug with AWS Tools layer creation
    Fixed a bug that prevented the creation of the AWS Tools layer when the staging directory did not exist.

--- powershell-modules/AWSToolsforPowerShell/Demo-AWS.Tools/build-AWSToolsLayer.ps1
+++ powershell-modules/AWSToolsforPowerShell/Demo-AWS.Tools/build-AWSToolsLayer.ps1
@@ -1,23 +1,30 @@
+$ProgressPreference = 'SilentlyContinue'
+
 #################
 # AWSToolsLayer #
 #################
 Write-Host "Downloading AWSToolsLayer" -foregroundcolor "green"
 Write-Host $PSScriptRoot
-Invoke-WebRequest  -Uri https://sdk-for-net.amazonwebservices.com/ps/v4/latest/AWS.Tools.zip -OutFile $PSScriptRoot\stage\AWS.Tools.zip
+
+$stagePath = Join-Path -Path $PSScriptRoot -ChildPath 'stage'
+$modulesPath = Join-Path -Path $PSScriptRoot -ChildPath 'modules'
+
+New-Item -Path $stagePath -ItemType Directory -Force | Out-Null
+Invoke-WebRequest -Uri https://sdk-for-net.amazonwebservices.com/ps/v4/latest/AWS.Tools.zip -OutFile $stagePath\AWS.Tools.zip
 
 ### Extract entire AWS.Tools modules to stage area
-#Write-Host 'Extracting full AWSTools module to: '$PSScriptRoot\modules
-#Expand-Archive $PSScriptRoot\stage\AWS.Tools.zip $PSScriptRoot\modules
+#Write-Host "Extracting full AWSTools module to: $modulesPath"
+#Expand-Archive -Path $stagePath\AWS.Tools.zip -DestinationPath $modulesPath
 
 ### Extract entire AWS.Tools modules to stage area but only move over select AWS.Tools modules (AWS.Tools.Common required)
-Write-Host "Extracting full AWSTools module to stage area:"$PSScriptRoot\stage -foregroundcolor "green"
-Expand-Archive $PSScriptRoot\stage\AWS.Tools.zip $PSScriptRoot\stage -Force
+Write-Host "Extracting full AWSTools module to stage area: $stagePath" -foregroundcolor "green"
+Expand-Archive -Path $stagePath\AWS.Tools.zip -DestinationPath $stagePath -Force
 
-Write-Host "Moving selected AWSTools modules to modules directory:"$PSScriptRoot\modules\ -foregroundcolor "green"
-If (!(Test-Path "$PSScriptRoot\modules\")) {New-Item -ItemType Directory -Force -Path "$PSScriptRoot\modules\" > $null}
-Move-Item "$PSScriptRoot\stage\AWS.Tools.Common" "$PSScriptRoot\modules\" -Force
-# Move-Item "$PSScriptRoot\stage\AWS.Tools.S3" "$PSScriptRoot\modules\" -Force
-# Move-Item "$PSScriptRoot\stage\AWS.Tools.EventBridge" "$PSScriptRoot\modules\" -Force
+Write-Host "Moving selected AWSTools modules to modules directory: $modulesPath" -foregroundcolor "green"
+New-Item -ItemType Directory -Force -Path $modulesPath | Out-Null
+Move-Item -Path "$stagePath\AWS.Tools.Common" -Destination $modulesPath -Force
+# Move-Item -Path "$stagePath\AWS.Tools.S3" -Destination $modulesPath -Force
+# Move-Item -Path "$stagePath\AWS.Tools.EventBridge" -Destination $modulesPath -Force
 
 Write-Host "Deleting AWSTools stage area" -foregroundcolor "green"
-Remove-Item $PSScriptRoot\stage -Recurse
+Remove-Item -Path $stagePath -Recurse
