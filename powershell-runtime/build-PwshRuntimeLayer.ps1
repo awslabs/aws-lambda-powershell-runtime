5c90fea9221e5448f32431c86397e8c4020b049f
Julian Wood <jrwood@amazon.com>
2023-01-19 19:26:51 +0000

    Amend runtime build process to improve PowerShell scrpting option

--- powershell-runtime/build-PwshRuntimeLayer.ps1
+++ powershell-runtime/build-PwshRuntimeLayer.ps1
@@ -3,21 +3,30 @@
 ####################
 Write-Host "Downloading Powershell for PwshRuntimeLayer" -foregroundcolor "green"
 Write-Host $PSScriptRoot
+# PWSH_VERSION is version of PowerShell to download
 $PWSH_VERSION = "7.2.4"
-$PWSH_ARCHITECTURE = "x64" #"x64" or #"arm64"
-Invoke-WebRequest  -Uri https://github.com/PowerShell/PowerShell/releases/download/v$PWSH_VERSION/powershell-$PWSH_VERSION-linux-$PWSH_ARCHITECTURE.tar.gz -OutFile  $PSScriptRoot\powershell-$PWSH_VERSION-$PWSH_ARCHITECTURE.tar.gz
+# PWSH_ARCHITECTURE can be 'x64' or 'arm64'
+$PWSH_ARCHITECTURE = "x64"
 
-Write-Host "Extracting Powershell"$PWSH_VERSION" for "$PWSH_ARCHITECTURE "to:" $PSScriptRoot/powershell -foregroundcolor "green"
-if (!(Test-Path "$PSScriptRoot\pwsh-runtime\powershell")) {New-Item -ItemType Directory -Force -Path "$PSScriptRoot\pwsh-runtime\powershell" > $null}
-tar zxf  $PSScriptRoot/powershell-$PWSH_VERSION-$PWSH_ARCHITECTURE.tar.gz -C  $PSScriptRoot/pwsh-runtime/powershell
+$sourcePath = Join-Path -Path $PSScriptRoot -ChildPath 'source'
+New-Item -Path $sourcePath -ItemType Directory -Force | Out-Null
+
+Invoke-WebRequest  -Uri https://github.com/PowerShell/PowerShell/releases/download/v$PWSH_VERSION/powershell-$PWSH_VERSION-linux-$PWSH_ARCHITECTURE.tar.gz -OutFile  $sourcePath\powershell-$PWSH_VERSION-$PWSH_ARCHITECTURE.tar.gz
+
+Write-Host "Extracting Powershell"$PWSH_VERSION" for "$PWSH_ARCHITECTURE "to:" $sourcePath -foregroundcolor "green"
+if (!(Test-Path "$sourcePath\powershell")) {New-Item -ItemType Directory -Force -Path "$sourcePath\powershell" > $null}
+tar zxf  $sourcePath/powershell-$PWSH_VERSION-$PWSH_ARCHITECTURE.tar.gz -C  $sourcePath/powershell
 
 Write-Host "Deleting PowerShell download" -foregroundcolor "green"
-Remove-Item  $PSScriptRoot/powershell-$PWSH_VERSION-$PWSH_ARCHITECTURE.tar.gz
+Remove-Item  $sourcePath/powershell-$PWSH_VERSION-$PWSH_ARCHITECTURE.tar.gz
 
-Write-Host "Merge all Private module content into a single .psm1 file to speed up module loading" -foregroundcolor "green"
-If (!(Select-String -Path $PSScriptRoot/pwsh-runtime/modules/pwsh-runtime.psm1 -Pattern 'private:Get-Handler')) {
-    Get-ChildItem -Path $PSScriptRoot/pwsh-runtime/modules/Private -Filter *.ps1 | ForEach-Object {Get-Content $_ | Select-Object -Skip 2 | Out-File -FilePath $PSScriptRoot/pwsh-runtime/modules/pwsh-runtime.psm1 -Append}
-    Remove-Item  $PSScriptRoot/pwsh-runtime/modules/Private -Force -Recurse
+Write-Host "Copy additional runtime files, including bootstrap. Remove Makefile from destination" -foregroundcolor "green"
+
+# cp -R ./pwsh-runtime/* $(ARTIFACTS_DIR)
+
+If (!(Select-String -Path $sourcePath/modules/pwsh-runtime.psm1 -Pattern 'private:Get-Handler')) {
+   Get-ChildItem -Path $sourcePath/modules/Private -Filter *.ps1 | ForEach-Object {Get-Content $_ | Select-Object -Skip 2 | Out-File -FilePath $sourcePath/modules/pwsh-runtime.psm1 -Append}
+   Remove-Item  $sourcePath/modules/Private -Force -Recurse
 } else {
-    Write-Host "Private modules likely merged already, skipping" -foregroundcolor "green"
+   Write-Host "Private modules likely merged already, skipping" -foregroundcolor "green"
 }
