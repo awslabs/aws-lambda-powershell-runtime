8acb3cdae47a01ed4f1fca70f9a5f76325b3d393
Andrew Pearce <andrew@austoonz.net>
2023-02-04 21:09:31 -0800

    Updates the runtime layer build script
    This change should resolve both https://github.com/awslabs/aws-lambda-powershell-runtime/issues/11 and https://github.com/awslabs/aws-lambda-powershell-runtime/issues/12.

--- powershell-runtime/build-PwshRuntimeLayer.ps1
+++ powershell-runtime/build-PwshRuntimeLayer.ps1
@@ -1,32 +1,43 @@
 ####################
 # PwshRuntimeLayer #
 ####################
-Write-Host "Downloading Powershell for PwshRuntimeLayer" -foregroundcolor "green"
+$ProgressPreference = 'SilentlyContinue'
+
+Write-Host "Downloading Powershell for PwshRuntimeLayer" -ForegroundColor 'Green'
 Write-Host $PSScriptRoot
-# PWSH_VERSION is version of PowerShell to download
+
+# PWSH_VERSION is version of PowerShell to use for the runtime
 $PWSH_VERSION = "7.2.4"
+
 # PWSH_ARCHITECTURE can be 'x64' or 'arm64'
 $PWSH_ARCHITECTURE = "x64"
 
 $sourcePath = Join-Path -Path $PSScriptRoot -ChildPath 'source'
-New-Item -Path $sourcePath -ItemType Directory -Force | Out-Null
 
-Invoke-WebRequest  -Uri https://github.com/PowerShell/PowerShell/releases/download/v$PWSH_VERSION/powershell-$PWSH_VERSION-linux-$PWSH_ARCHITECTURE.tar.gz -OutFile  $sourcePath\powershell-$PWSH_VERSION-$PWSH_ARCHITECTURE.tar.gz
+$modulePath = Join-Path -Path $sourcePath -ChildPath 'modules'
+$moduleFilePath = Join-Path -Path $modulePath -ChildPath 'pwsh-runtime.psm1'
+$privateFunctionPath = Join-Path -Path $modulePath -ChildPath 'Private'
 
-Write-Host "Extracting Powershell"$PWSH_VERSION" for "$PWSH_ARCHITECTURE "to:" $sourcePath -foregroundcolor "green"
-if (!(Test-Path "$sourcePath\powershell")) {New-Item -ItemType Directory -Force -Path "$sourcePath\powershell" > $null}
-tar zxf  $sourcePath/powershell-$PWSH_VERSION-$PWSH_ARCHITECTURE.tar.gz -C  $sourcePath/powershell
+$powershellPath = Join-Path -Path $sourcePath -ChildPath 'powershell'
+$tarFile = Join-Path -Path $sourcePath -ChildPath "powershell-$PWSH_VERSION-$PWSH_ARCHITECTURE.tar.gz"
 
-Write-Host "Deleting PowerShell download" -foregroundcolor "green"
-Remove-Item  $sourcePath/powershell-$PWSH_VERSION-$PWSH_ARCHITECTURE.tar.gz
+$githubSource = "https://github.com/PowerShell/PowerShell/releases/download/v$PWSH_VERSION/powershell-$PWSH_VERSION-linux-$PWSH_ARCHITECTURE.tar.gz"
+Invoke-WebRequest -Uri $githubSource -OutFile $tarFile
 
-Write-Host "Copy additional runtime files, including bootstrap. Remove Makefile from destination" -foregroundcolor "green"
+Write-Host "Extracting Powershell $PWSH_VERSION for $PWSH_ARCHITECTURE to $powershellPath" -ForegroundColor 'Green'
+if (-not(Test-Path -Path $powershellPath)) {$null = New-Item -ItemType Directory -Force -Path $powershellPath}
+tar zxf $tarFile -C $powershellPath
 
-# cp -R ./pwsh-runtime/* $(ARTIFACTS_DIR)
+Write-Host "Deleting PowerShell download" -ForegroundColor 'Green'
+Remove-Item -Path $tarFile -Force
 
-If (!(Select-String -Path $sourcePath/modules/pwsh-runtime.psm1 -Pattern 'private:Get-Handler')) {
-   Get-ChildItem -Path $sourcePath/modules/Private -Filter *.ps1 | ForEach-Object {Get-Content $_ | Select-Object -Skip 2 | Out-File -FilePath $sourcePath/modules/pwsh-runtime.psm1 -Append}
-   Remove-Item  $sourcePath/modules/Private -Force -Recurse
+Write-Host "Copy additional runtime files, including bootstrap. Remove Makefile from destination" -ForegroundColor 'Green'
+
+if (-not(Select-String -Path $moduleFilePath -Pattern 'private:Get-Handler')) {
+    Get-ChildItem -Path $privateFunctionPath -Filter '*.ps1' | ForEach-Object {
+        Get-Content $_.FullName | Select-Object -Skip 2 | Out-File -FilePath $moduleFilePath -Append -Encoding utf8
+    }
+    Remove-Item -Path $privateFunctionPath -Force -Recurse
 } else {
-   Write-Host "Private modules likely merged already, skipping" -foregroundcolor "green"
+    Write-Host "Private modules likely merged already, skipping" -ForegroundColor 'Green'
 }
