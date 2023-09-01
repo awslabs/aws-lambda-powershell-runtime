5ff7c7df7a85797ab295a5ecde805d8619b0db5b
Andrew Pearce <andrew@austoonz.net>
2023-09-01 17:45:10 +0000

    Updated example to use PowerShell x64.

--- examples/demo-runtime-layer-function/build-layers.ps1
+++ examples/demo-runtime-layer-function/build-layers.ps1
@@ -9,7 +9,7 @@ $layersRoot = Join-Path -Path $PSScriptRoot -ChildPath 'layers'
 ####################
 $runtimeLayerPath = Join-Path -Path $layersRoot -ChildPath 'runtimeLayer'
 $runtimeBuildScript = [System.IO.Path]::Combine($gitRoot, 'powershell-runtime', 'build-PwshRuntimeLayer.ps1')
-& $runtimeBuildScript -PwshArchitecture 'arm64' -LayerPath $runtimeLayerPath
+& $runtimeBuildScript -PwshArchitecture 'x64' -LayerPath $runtimeLayerPath
 
 #################
 # AWSToolsLayer #
