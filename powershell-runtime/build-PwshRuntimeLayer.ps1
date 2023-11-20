51e3fc8aea6cb4c615715c04745ccb0e3c9e1c0e
Andrew Pearce <pearca@amazon.com>
2023-11-20 12:04:45 -0800

    Updated to PowerShell 7.4.0 by default

--- powershell-runtime/build-PwshRuntimeLayer.ps1
+++ powershell-runtime/build-PwshRuntimeLayer.ps1
@@ -4,7 +4,7 @@
 #>
 param (
     # The version of PowerShell to use for the runtime
-    [string]$PwshVersion = '7.2.13',
+    [string]$PwshVersion = '7.4.0',
 
     # The desired CPU architecture for the runtime
     [ValidateSet('arm64', 'x64')]
